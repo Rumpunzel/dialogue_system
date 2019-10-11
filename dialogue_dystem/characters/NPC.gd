178f98ae43bc9d678bd9a8e76a7486e4132ee879
Rumpunzel <bencharki.jamal@gmail.com>
2019-10-11 13:16:14 +0200

    added approval rating an perception architecture

--- dialogue_dystem/characters/NPC.gd
+++ dialogue_dystem/characters/NPC.gd
@@ -1,12 +1,38 @@
 extends Character
 class_name NPC
 
-onready var personal_values:character_values = character_values.new(politeness, reliability, selflessness, sincerity)
+onready var personal_values = [politeness, reliability, selflessness, sincerity]
+
+var character_perceptions:Dictionary
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+# pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func modify_perception(target:Character, value_changes, _big_deal):
+	character_perceptions[target] = character_perceptions.get(target, target.percieved_starting_values)
+	character_perceptions[target] = math_helper.vector_add_arrays([character_perceptions[target], value_changes])
+	
+	print("New Values for %s towards %s: %s, %s" % [name, target.name, character_perceptions[target], calculate_perception_value(character_perceptions[target])])
+	print("Approval Rating of %s towards %s is now: %f" % [name, target.name, calculate_approval_rating(target)])
+	
+
+func calculate_approval_rating(target:Character):
+	var approval_rating = 0
+	
+	if not character_perceptions.get(target, null) == null:
+		var perception_values = calculate_perception_value(character_perceptions[target])
+		for i in personal_values.size():
+			var approval_change = perception_values[i] * personal_values[i]
+			
+			if not approval_change == 0:
+				print(("+" if approval_change >= 0 else "") + str(approval_change) + " from " + VALUE_NAMES[i])
+			approval_rating += approval_change
+	
+	return approval_rating
