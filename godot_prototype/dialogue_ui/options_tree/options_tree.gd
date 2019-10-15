6eb93b0596d635e7dd5edefc3417a80d0bcbd8a5
Rumpunzel <bencharki.jamal@gmail.com>
2019-10-15 13:17:30 +0200

    simplified adding of options

--- godot_prototype/dialogue_ui/options_tree/options_tree.gd
+++ godot_prototype/dialogue_ui/options_tree/options_tree.gd
@@ -14,33 +14,46 @@ func _process(_delta):
 	pass
 
 
-func add_option(option_id:String, option_type):
-	var speaker_check = option_type.get("speaker", get_default_speaker())
-	var listeners_check = option_type.get("listeners", get_default_listeners())
+func add_option(option_id:String, option_type = null):
+	var speaker_check = get_default_speaker()
+	var listeners_check = get_default_listeners()
 	
 	var success_counter = 0
 	var failure_counter = 0
 	
-	if not option_id.begins_with("_"):
-		for listener in listeners_check:
-			var recollection = listener.remembers_dialogue_option(option_id)
-			
-			if not recollection == null and recollection["speaker"] == speaker_check:
-				success_counter = recollection["success_counter"]
-				failure_counter = recollection["failure_counter"]
+	if not option_type == null:
+		speaker_check = option_type.get("speaker", speaker_check)
+		listeners_check = option_type.get("listeners", listeners_check)
+		
+		if not option_id.begins_with("_"):
+			for listener in listeners_check:
+				var recollection = listener.remembers_dialogue_option(option_id)
+				
+				if not recollection == null and recollection["speaker"] == speaker_check:
+					success_counter = recollection["success_counter"]
+					failure_counter = recollection["failure_counter"]
 	
 	var new_option = dialogue_option_scene.instance()
 	new_option.speaker = speaker_check
 	new_option.listeners = listeners_check
 	new_option.success_counter = success_counter
 	new_option.failure_counter = failure_counter
-	new_option.init(option_id, option_type)
 	
-	if not option_type.get("single_use", true) or (success_counter == 0 and failure_counter == 0) or (option_type.get("big_deal", false) and success_counter == 0 and new_option.check_success() >= dialogue_option.PASSED):
-		add_child(new_option)
-		new_option.connect("option_confirmed", self, "choice_made")
+	if not option_type == null:
+		new_option.init(option_id, option_type)
+		
+		if not option_type.get("single_use", true) or (success_counter == 0 and failure_counter == 0) or (option_type.get("big_deal", false) and success_counter == 0 and new_option.check_success() >= dialogue_option.PASSED):
+			add_option_to_tree(new_option)
+		else:
+			new_option.queue_free()
 	else:
-		new_option.queue_free()
+		new_option.init(option_id)
+		
+		add_option_to_tree(new_option)
+
+func add_option_to_tree(new_option):
+	add_child(new_option)
+	new_option.connect("option_confirmed", self, "choice_made")
 
 func choice_made(updated_info):
 	clear_options()
