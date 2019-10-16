58577361a6b16d2fb3df31cbc2381fd2f72f8b9e
Rumpunzel <bencharki.jamal@gmail.com>
2019-10-16 14:51:40 +0200

    streamlined and imprved perception value UI

--- dialogue_edtitor/dialogue_editor/properties/perception_values.gd
+++ dialogue_edtitor/dialogue_editor/properties/perception_values.gd
@@ -2,14 +2,25 @@ extends GridContainer
 
 export(PackedScene) var value_slider = preload("res://dialogue_editor/properties/value_slider.tscn")
 
+export(NodePath) var perception_graph = null
+
+var slider_values:Dictionary
+
+signal update_perceptions_graph
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if not perception_graph == null:
+		connect("update_perceptions_graph", get_node(perception_graph), "update_perceptions_graph")
+	
 	for value in GAME_CONSTANTS._PERCEPTION_VALUES:
 		var value_name = Label.new()
 		value_name.text = value.capitalize()
 		
 		var slider = value_slider.instance()
-		#slider.set_property_name(value)
+		slider.value_name = value
+		slider.connect("value_changed", self, "update_perceptions_graph")
 		
 		add_child(value_name)
 		add_child(slider)
@@ -17,3 +28,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func update_perceptions_graph(value_name:String, new_value):
+	slider_values[value_name] = new_value
+	
+	emit_signal("update_perceptions_graph", slider_values, true)
--- dialogue_edtitor/dialogue_editor/properties/value_slider.gd
+++ dialogue_edtitor/dialogue_editor/properties/value_slider.gd
@@ -1,7 +1,26 @@
 extends HBoxContainer
 
-func set_property_value(new_value:int):
-	$spin_box.value = new_value
+var value_name:String setget set_value_name, get_value_name
+
+signal value_changed
+
+
+func update_value(new_value, node_name = null):
+	set_property_value(new_value, node_name)
+	emit_signal("value_changed", value_name, new_value)
+
+
+func set_property_value(new_value:int, node_name = null):
+	for child in get_children():
+		if not child.name == node_name:
+			child.value = new_value
+
+func set_value_name(new_name):
+	value_name = new_name
+
 
 func get_property_value() -> int:
 	return $spin_box.value
+
+func get_value_name() -> String:
+	return value_name
