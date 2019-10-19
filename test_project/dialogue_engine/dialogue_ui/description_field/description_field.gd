641f15f2b68b11a6b6f07ae8ad420d81e806a67a
Rumpunzel <bencharki.jamal@gmail.com>
2019-10-19 09:11:35 +0200

    replaced hard links with symbolic links

--- test_project/dialogue_engine/dialogue_ui/description_field/description_field.gd
+++
@@ -1,8 +0,0 @@
-extends TypingLabel
-
-
-func update_description(update:Dictionary):
-	var new_description = update.get("message", "")
-	
-	if not new_description == "":
-		type_text(new_description)
