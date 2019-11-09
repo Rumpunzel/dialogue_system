dd88613ef5e1eadce33a09a933ef6264fa6fd320
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-09 11:13:49 +0100

    added confirmation button

---
+++ dialogue_edtitor/dialogue_editor/properties/button_with_confirmation.gd
@@ -0,0 +1,39 @@
+extends Button
+class_name ButtonWithConfirmation
+
+export(String, MULTILINE) var confirmation_text
+
+signal confirmed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", self, "open_dialog")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func open_dialog():
+	var dialog = create_dialog()
+	
+	dialog.connect("confirmed", self, "confirm_dialog")
+	
+	dialog.popup_centered()
+
+func create_dialog():
+	var dialog = ConfirmationDialog.new()
+	
+	dialog.dialog_text = confirmation_text
+	dialog.popup_exclusive = true
+	
+	dialog.get_label().align = Label.ALIGN_CENTER
+	dialog.get_label().valign = Label.VALIGN_CENTER
+	
+	add_child(dialog)
+	
+	return dialog
+
+func confirm_dialog():
+	emit_signal("confirmed")
