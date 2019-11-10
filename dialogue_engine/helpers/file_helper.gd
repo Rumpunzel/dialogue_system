20b13567f89f80907f0ebfca91c7c8b0b6570623
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-10 17:00:18 +0100

    file helper now can delete files and return entire file paths instead of only file names

--- dialogue_engine/helpers/file_helper.gd
+++ dialogue_engine/helpers/file_helper.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-func list_files_in_directory(directory_path:String, recursive:bool, directory_as_key:String = ""):
+func list_files_in_directory(directory_path:String, recursive:bool, file_suffix:String = ""):
 	var files = []
 	var directory = Directory.new()
 	
@@ -15,13 +15,17 @@ func list_files_in_directory(directory_path:String, recursive:bool, directory_as
 			break
 		elif directory.current_is_dir():
 			if recursive:
-				files.append(list_files_in_directory(directory_path.plus_file(file), recursive, file))
-		else:
-			files.append(file)
+				files += list_files_in_directory(directory_path.plus_file(file), recursive, file_suffix)
+		elif file.ends_with(file_suffix):
+			files.append(directory_path.plus_file(file))
 			
 			if CONSTANTS.verbose_mode:
 				CONSTANTS.print_to_console("Found file %s in directory %s" % [file, directory_path])
 	
 	directory.list_dir_end()
 	
-	return files if directory_as_key.length() == 0 else { directory_as_key: files }
+	return files
+
+func delete_file(file_path:String):
+	var directory = Directory.new()
+	directory.remove(file_path)
