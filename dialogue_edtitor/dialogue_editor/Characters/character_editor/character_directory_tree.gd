4321f43ceee74d2de1fa55a4f24d1a0018067871
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-10 12:20:53 +0100

    deleted unused files and reorganized project structure a bit

--- dialogue_edtitor/dialogue_editor/Characters/character_editor/character_directory_tree.gd
+++ dialogue_edtitor/dialogue_editor/Characters/character_editor/character_directory_tree.gd
@@ -1,4 +1,5 @@
 extends directory_tree
+class_name character_directory_tree
 
 func load_entries():
 	return json_helper.load_json(Character.json_paths[Character.STATS_PATHS.MODIFIED]).keys()
--- dialogue_edtitor/dialogue_editor/properties/CharacterEditor/character_directory/character_entry.gd
+++
@@ -1,5 +0,0 @@
-extends directory_entry
-
-
-func _on_open_pressed():
-	get_node("/root/main/editor/Characters").open_new_tab($name.text)
