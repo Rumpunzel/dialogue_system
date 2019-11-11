c65dace4ef98ecc4ff94d027de551a55b5d7f4ad
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-11 08:19:10 +0100

    added grouping and filtering ui to default editor

--- dialogue_edtitor/dialogue_editor/utility/directory_tree.gd
+++ dialogue_edtitor/dialogue_editor/utility/directory_tree.gd
@@ -1,6 +1,10 @@
 extends Tree
 class_name directory_tree
 
+enum { NAME, TAGS, PATH }
+
+const COLUMNS = { NAME: "Name", TAGS: "Tags", PATH: "Path" }
+
 export(String, DIR) var entry_directory
 
 export(String) var file_ending
@@ -17,7 +21,11 @@ func _ready():
 	entries = file_helper.list_files_in_directory(entry_directory, true, file_ending)
 	
 	root = create_item()
-	root.set_text(0, entry_directory.get_file())
+	
+	for column in columns:
+		set_column_title(column, COLUMNS.get(column, ""))
+	
+	set_column_titles_visible(true)
 	
 	parse_tree(entries, root)
 	
@@ -25,25 +33,18 @@ func _ready():
 	#get_node(root_node).connect("tab_changed", self, "_tab_changed")
 
 
-func parse_tree(options, root_entry, column_offset = 0):
+func parse_tree(options, root_entry):
 	var keys = options.keys() if typeof(options) == TYPE_DICTIONARY else options.size()
 	
 	for option in keys:
 		var data = options[option]
+		var entry = create_item(root_entry)
 		
-		if not data.get_base_dir().get_file() == root_entry.get_text(column_offset).get_file():
-			var entry = create_item(root_entry)
-			
-			entry.set_text(column_offset, str(data).get_base_dir().get_file())
-			parse_tree([data], entry, column_offset)
-		else:
-			var entry = create_item(root_entry)
-			
-			entry.set_text(column_offset, str(data).get_file().trim_suffix(file_ending))
-			entry.set_text(column_offset + 1, str(data))
+		entry.set_text(NAME, str(data).get_file().trim_suffix(file_ending).capitalize())
+		entry.set_text(PATH, str(data))
 
 func open_entry(node = get_node(root_node)):
-	node.open_new_tab(get_selected().get_text(1), get_selected().get_text(0))
+	node.open_new_tab(get_selected().get_text(PATH), get_selected().get_text(NAME))
 
 
 func _tab_changed(_index):
