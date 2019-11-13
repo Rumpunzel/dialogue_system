92f012ef3f891cc82c49a316e42c3a6224200e38
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-13 11:56:22 +0100

    made directory tree a little more modular

--- dialogue_edtitor/dialogue_editor/utility/directory/directory_tree.gd
+++ dialogue_edtitor/dialogue_editor/utility/directory/directory_tree.gd
@@ -10,7 +10,11 @@ const COLUMNS = { NAME: "Name", TAGS: "Tags", PATH: "Path" }
 
 export(NodePath) var root_node
 
+export(bool) var show_tags = true
+
 onready var root = get_node(root_node)
+
+onready var path_column = PATH - (0 if show_tags else 1)
 
 var entries
 var entry_map:Dictionary
@@ -27,6 +31,7 @@ func _ready():
 	
 	for column in columns:
 		set_column_title(column, COLUMNS.get(column, ""))
+	set_column_title(path_column, COLUMNS[PATH])
 	
 	set_column_titles_visible(true)
 	
@@ -35,6 +40,16 @@ func _ready():
 	connect("item_activated", self, "open_entry")
 	#get_node(root_node).connect("tab_changed", self, "_tab_changed")
 
+
+
+func group_tree(group_by, filter = ""):
+	match group_by:
+		NOTHING:
+			parse_tree(entries, tree_root, filter)
+		FOLDERS:
+			parse_tree(entries, tree_root, filter, PATH)
+		_:
+			parse_tree(entries, tree_root, filter, TAGS, group_by)
 
 
 func parse_tree(options, root_entry, filter, group_by = null, category = null):
@@ -85,11 +100,12 @@ func parse_branch(branch:Array, root_entry, full_path, filter, tags_dictionary:D
 		entry = entry_map.get(node_name)
 	
 	if branch.empty():
-		entry.set_text(PATH, full_path.trim_prefix(root.entry_directory + "/"))
-		entry.set_metadata(PATH, full_path)
+		entry.set_text(path_column, full_path.trim_prefix(root.entry_directory + "/"))
+		entry.set_metadata(path_column, full_path)
 		
-		entry.set_text(TAGS, extract_tags_from_array(tags_dictionary.values()))
-		entry.set_metadata(TAGS, str(tags_dictionary))
+		if show_tags:
+			entry.set_text(TAGS, extract_tags_from_array(tags_dictionary.values()))
+			entry.set_metadata(TAGS, str(tags_dictionary))
 		
 		for tag in tags_dictionary:
 			if not tag in groups:
@@ -98,28 +114,21 @@ func parse_branch(branch:Array, root_entry, full_path, filter, tags_dictionary:D
 		parse_branch(branch, entry, full_path, filter, tags_dictionary)
 
 
-func group_tree(group_by, filter = ""):
-	match group_by:
-		NOTHING:
-			parse_tree(entries, tree_root, filter)
-		FOLDERS:
-			parse_tree(entries, tree_root, filter, PATH)
-		_:
-			parse_tree(entries, tree_root, filter, TAGS, group_by)
-
-
 func open_entry(node = get_node(root_node).get_root_node()):
-	node.open_new_tab(get_selected().get_metadata(PATH), get_selected().get_text(NAME))
+	node.open_new_tab(get_selected().get_metadata(path_column), get_selected().get_text(NAME))
 
 
 func extract_tags_from_array(array):
 	var return_string = ""
-	
+	print(array)
 	for entry in array:
-		var tags = entry.split("/", false)
-		
-		for tag in tags:
-			return_string += "%s, " % [tag]#.capitalize()]
+		if not typeof(entry) == TYPE_ARRAY:
+			var tags = entry.split("/", false)
+			
+			for tag in tags:
+				return_string += "%s, " % [tag]
+		else:
+			return_string += "%s, " % [extract_tags_from_array(entry)]
 	
 	return return_string.trim_suffix(", ")
 
