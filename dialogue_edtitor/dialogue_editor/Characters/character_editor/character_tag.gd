ccc67cefdcd1b167945afc42fde72d604cc7a535
Rumpunzel <bencharki.jamal@gmail.com>
2019-11-13 12:11:15 +0100

    made character tags modular

--- dialogue_edtitor/dialogue_editor/Characters/character_editor/character_tag.gd
+++ dialogue_edtitor/dialogue_editor/Characters/character_editor/character_tag.gd
@@ -1,20 +1,6 @@
 extends PanelContainer
 
-export(String) var tag_name
 
-export(NodePath) var root_node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_node(root_node).connect("current_NPC", self, "setup_tree")
-	
+func setup_tree(NPC, tag_name):
 	$tag_container/label.text = tag_name
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func setup_tree(NPC):
 	$tag_container/reference_tree.setup(NPC, tag_name)
