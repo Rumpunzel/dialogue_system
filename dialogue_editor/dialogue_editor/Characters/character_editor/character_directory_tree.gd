6a2142bfa77a7784d2e2eb763e23b40570fe253b
Rumpunzel <bencharki.jamal@gmail.com>
2020-03-21 17:37:25 +0100

    deleted entire repo

--- dialogue_editor/dialogue_editor/Characters/character_editor/character_directory_tree.gd
+++
@@ -1,5 +0,0 @@
-extends directory_tree
-
-
-func open_entry(node = get_node(root_node).get_root_node()):
-	.open_entry(node.get_parent())
--- dialogue_editor/dialogue_editor/Characters/character_editor/character_tag.gd
+++
@@ -1,9 +0,0 @@
-extends tag
-
-
-func setup_tree(NPC, tag_name):
-	var label = $tag_container/button_divider/label
-	var reference_tree = $tag_container/reference_tree
-	
-	label.text = tag_name
-	reference_tree.setup(NPC, tag_name)
